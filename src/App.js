589384942c1dda77702bb1800581bc4fdb64f723
Andrew <atklenner@gmail.com>
2022-06-09 21:07:38 -0700

    finish 1.10

--- src/App.js
+++ src/App.js
@@ -4,37 +4,49 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  let total = good + neutral + bad;
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button handler={() => setGood(good + 1)} text="good" />
+      <Button handler={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handler={() => setBad(bad + 1)} text="bad" />
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {total}</p>
-      <Statistics good={good} bad={bad} total={total} />
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
 
-const Statistics = ({ good, bad, total }) => {
+const Statistics = ({ good, bad, neutral }) => {
+  let total = good + neutral + bad;
   return (
     <div>
       {total > 0 ? (
         <div>
-          <p>average {(good - bad) / total}</p>
-          <p>positive {good / total}</p>
+          <StatisticsLine text="good" value={good} />
+          <StatisticsLine text="neutral" value={neutral} />
+          <StatisticsLine text="bad" value={bad} />
+          <StatisticsLine text="all" value={total} />
+          <StatisticsLine text="average" value={(good - bad) / total} />
+          <StatisticsLine text="positive" value={good / total} />
         </div>
       ) : (
         <p>No feedback given</p>
       )}
     </div>
   );
+};
+
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <p>
+      {text} {value}
+    </p>
+  );
+};
+
+const Button = ({ text, handler }) => {
+  return <button onClick={handler}>{text}</button>;
 };
 
 export default App;
