d747a6c8a1f84f92a3a23e39dd3fb545fcbbb3f3
Andrew <atklenner@gmail.com>
2022-06-09 21:43:02 -0700

    finish 1.12

--- src/App.js
+++ src/App.js
@@ -1,53 +1,30 @@
 import { useState } from "react";
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    "If it hurts, do it more often",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients",
+  ];
+
+  const [selected, setSelected] = useState(0);
+
+  function handleClick() {
+    let random = Math.floor(Math.random() * anecdotes.length);
+    setSelected(random);
+  }
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <Button handler={() => setGood(good + 1)} text="good" />
-      <Button handler={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handler={() => setBad(bad + 1)} text="bad" />
-      <h1>statistics</h1>
-      <Statistics good={good} bad={bad} neutral={neutral} />
+      {anecdotes[selected]}
+      <br></br>
+      <button onClick={handleClick}>next anecdote</button>
     </div>
   );
-};
-
-const Statistics = ({ good, bad, neutral }) => {
-  let total = good + neutral + bad;
-  return (
-    <div>
-      {total > 0 ? (
-        <div>
-          <StatisticsLine text="good" value={good} />
-          <StatisticsLine text="neutral" value={neutral} />
-          <StatisticsLine text="bad" value={bad} />
-          <StatisticsLine text="all" value={total} />
-          <StatisticsLine text="average" value={(good - bad) / total} />
-          <StatisticsLine text="positive" value={good / total} />
-        </div>
-      ) : (
-        <p>No feedback given</p>
-      )}
-    </div>
-  );
-};
-
-const StatisticsLine = ({ text, value }) => {
-  return (
-    <tr>
-      <td>{text}</td>
-      <td>{value}</td>
-    </tr>
-  );
-};
-
-const Button = ({ text, handler }) => {
-  return <button onClick={handler}>{text}</button>;
 };
 
 export default App;
